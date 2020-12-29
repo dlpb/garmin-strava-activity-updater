5b7157e232be8b339c2a24c17d271cd223fdf4e3
Daniel Brown <dlpb@outlook.com>
2020-12-29 17:10:37 +0000

    tie it all together

--- src/main/java/uk/dlpb/App.java
+++ src/main/java/uk/dlpb/App.java
@@ -1,4 +1,22 @@
 package uk.dlpb;
 
+import uk.dlpb.model.GarminActivity;
+import uk.dlpb.model.Params;
+import uk.dlpb.model.StravaActivity;
+import uk.dlpb.processor.ActivityProcessor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class App {
+    public static void main(String[] params) throws IOException {
+        Params appParams = new Params(params);
+
+        Stream<StravaActivity> stravaActivities = StravaActivity.fromFile(Path.of(appParams.getStravaActivitiesFilePath())).stream();
+        Stream<GarminActivity> garminActivities = GarminActivity.fromFile(Path.of(appParams.getGarminActivitiesFilePath())).stream();
+
+        new ActivityProcessor().process(stravaActivities, garminActivities, appParams.getStravaFilterGear(), appParams.getGarminToken(), appParams.getGarminGearToRemove(), appParams.getGarminGearToAdd());
+
+    }
 }
